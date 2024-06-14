5e3f4b875cabbe37a187a1af219de81e831b3dfe
linuxguy467 <me@matthem.codes>
2024-06-14 19:31:36 -0400

    Add avatar image

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,19 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import logo from '../img/logo.png';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 
 const Navbar = () => {
   return (
-    <div className='bg-primary dark:bg-slate-700 text-white py-2 px-5 flexbox justify-between'>
+    <div className='bg-primary dark:bg-slate-700 text-white py-2 px-5 flex justify-between'>
       <Link href='/'>
         <Image src={logo} alt='MatthemPress' width={40} />
       </Link>
+
+      <Avatar>
+        <AvatarImage src='https://github.com/shadcn.png' alt='@shadcn' />
+        <AvatarFallback className='text-black'>MH</AvatarFallback>
+      </Avatar>
     </div>
   );
 };
